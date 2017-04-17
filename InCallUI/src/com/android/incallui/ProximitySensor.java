5f915bfab280a8e9ec72baca95657b46792fc3aa
rc420head <dennislebhardt@yahoo.com>
2017-04-16 23:57:21 -0500

    Revert "Dialer: New slim framework package name"
    This reverts commit 4a282d3c9effba43d4cd4742e11937898d36918b.

--- InCallUI/src/com/android/incallui/ProximitySensor.java
+++ InCallUI/src/com/android/incallui/ProximitySensor.java
@@ -35,7 +35,7 @@ import com.android.incallui.AudioModeProvider.AudioModeListener;
 import com.android.incallui.InCallPresenter.InCallState;
 import com.android.incallui.InCallPresenter.InCallStateListener;
 
-import slim.provider.SlimSettings;
+import org.slim.provider.SlimSettings;
 
 /**
  * Class manages the proximity sensor for the in-call UI.
